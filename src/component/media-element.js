59a0a931d2b2f19082fdfc275600f5ead83997f3
scenaristeur@gmail.com <scenaristeur@gmail.com>
2019-12-17 13:09:21 +0100

    test media

--- src/component/media-element.js
+++ src/component/media-element.js
@@ -24,10 +24,61 @@ class MediaElement extends LitElement {
 
   render(){
     return html`
+    <link href="css/fontawesome/css/all.css" rel="stylesheet">
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    i {
+      padding: 10px
+    }
+    </style>
     <p>${this.name}</p>
     list : ${this.notesListUrl}
+    <div class="row">
+    <form>
+    <!--https://www.html5rocks.com/en/tutorials/getusermedia/intro/-->
+    <!--            <div class="custom-file">
+    <input type="file" class="custom-file-input" @change="${this.createTemp}" id="imageFile" accept="image/*;capture=camera" lang="${this.lang}">
+    <label class="custom-file-label" for="imageFile"><i class="fas fa-camera-retro"></i> Image</label>
+    </div>
+    <div class="custom-file">
+    <input type="file" class="custom-file-input" @change="${this.createTemp}" id="videoFile" accept="video/*;capture=camcorder" lang="${this.lang}">
+    <label class="custom-file-label" for="videoFile"><i class="fas fa-video"></i> Video</label>
+    </div>
+    <div class="custom-file">
+    <input type="file" class="custom-file-input" @change="${this.createTemp}" id="audioFile" accept="audio/*;capture=microphone" lang="${this.lang}">
+    <label class="custom-file-label" for="audioFile"><i class="fas fa-microphone"></i> Audio</label>
+    </div>
+    -->
+    <div class="custom-file">
+    <input type="file" class="custom-file-input" @change="${this.createTemp}" id="audioFile" accept="image/*;video/*;audio/*" lang="${this.lang}">
+    <label class="custom-file-label" for="audioFile"><i class="fas fa-camera-retro"></i><i class="fas fa-video"></i><i class="fas fa-microphone"></i></label>
+    </div>
+    </form>
+    </div>
 
-    <button @click="${this.sendMessage}">Send message</button>
+
+    Folder : <a href="${this.path}" target="_blank">${this.path}</a>
+
+    <div class="col-auto">
+    <label class="sr-only" for="filename">Filename</label>
+    <div class="input-group mb-2">
+    <input id="filename" class="form-control" type="text" value="${this.filename}" @change="${this.filenameChange}" placeholder="Filename">
+    <div class="input-group-append">
+    <div class="input-group-text">${this.extension}</div>
+    </div>
+    </div>
+    </div>
+
+    <!--
+    <input id="filename" class="form-control" type="text" value="${this.filename}" @change="${this.filenameChange}" placeholder="Filename"> ${this.extension}
+    -->
+
+
+
+    <div class="col-auto"><canvas style="max-width: 100%; height: auto;" id="canvas"/></div>
+
+
     `;
   }
 
@@ -45,8 +96,8 @@ class MediaElement extends LitElement {
         }
       }
     };
-      this.ph = new PodHelper("bip",12);
-      console.log("PH VALUE",this.ph.count)
+    this.ph = new PodHelper("bip",12);
+    console.log("PH VALUE",this.ph.count)
     var auth  =   JSON.parse(localStorage.getItem('solid-auth-client'));
     this.webId = auth.session.webId
     console.log(this.webId)
@@ -55,50 +106,50 @@ class MediaElement extends LitElement {
 
   init(){
     var app = this;
-  fetchDocument(app.webId).then(
-    doc => {
-      app.doc = doc;
-      app.person = doc.getSubject(app.webId);
-      app.initNotePod()
-    },
-    err => {
-      console.log(err)
-    }
-  );
+    fetchDocument(app.webId).then(
+      doc => {
+        app.doc = doc;
+        app.person = doc.getSubject(app.webId);
+        app.initNotePod()
+      },
+      err => {
+        console.log(err)
+      }
+    );
   }
 
 
-initNotePod(){
-  var app = this
-  this.publicTypeIndexUrl = this.person.getRef(solid.publicTypeIndex)
-  console.log("INIT publicTypeIndexUrl",this.publicTypeIndexUrl)
-  fetchDocument(this.publicTypeIndexUrl).then(
-    publicTypeIndex => {
-      app.publicTypeIndex = publicTypeIndex;
-      app.notesListEntry = app.publicTypeIndex.findSubject(solid.forClass, schema.TextDigitalDocument);
-      //  console.log("app.notesListEntry",app.notesListEntry)
-      if (app.notesListEntry === null){
-        app.notesListUrl = app.initialiseNotesList(app.person, app.publicTypeIndex)
-      }else{
-        app.notesListUrl = app.notesListEntry.getRef(solid.instance)
-        //  console.log("notesListUrl",app.notesListUrl)
-      }
-    //  app.getNotes()
-    fetchDocument(app.notesListUrl).then(
-      notesList => {
-        app.notesList = notesList;
-         console.log("app.notesList",app.notesList)
-      })
-    },
-    err => {console.log(err)}
-  );
-}
+  initNotePod(){
+    var app = this
+    this.publicTypeIndexUrl = this.person.getRef(solid.publicTypeIndex)
+    console.log("INIT publicTypeIndexUrl",this.publicTypeIndexUrl)
+    fetchDocument(this.publicTypeIndexUrl).then(
+      publicTypeIndex => {
+        app.publicTypeIndex = publicTypeIndex;
+        app.notesListEntry = app.publicTypeIndex.findSubject(solid.forClass, schema.TextDigitalDocument);
+        //  console.log("app.notesListEntry",app.notesListEntry)
+        if (app.notesListEntry === null){
+          app.notesListUrl = app.initialiseNotesList(app.person, app.publicTypeIndex)
+        }else{
+          app.notesListUrl = app.notesListEntry.getRef(solid.instance)
+          //  console.log("notesListUrl",app.notesListUrl)
+        }
+        //  app.getNotes()
+        fetchDocument(app.notesListUrl).then(
+          notesList => {
+            app.notesList = notesList;
+            console.log("app.notesList",app.notesList)
+          })
+        },
+        err => {console.log(err)}
+      );
+    }
 
 
-  personChanged(person){
-    this.person = person
-    console.log(person)
-  /*  if (person != null){
+    personChanged(person){
+      this.person = person
+      console.log(person)
+      /*  if (person != null){
       console.log(person.storage)
       this.initNotePod()
     }*/
